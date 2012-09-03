c0d2a99e1eed6e6fe7fb9cf67f915d0b78e37858
Colin Powell <colin.powell@gmail.com>
2012-09-03 02:00:49 -0400

    FIX: Also removing form hookup from the admin file for superslides.

--- apps/superslides/admin.py
+++ apps/superslides/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from superslides.forms import SlideAdminForm
 
 from superslides.models import Slide
 
 class SlideAdmin(admin.ModelAdmin):
     list_display= ('name', 'caption', 'published',)
     list_filter= ('published',)
-    form = SlideAdminForm
 
 admin.site.register(Slide, SlideAdmin)
