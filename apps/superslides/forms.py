4f9592572ebe0ff406dea67717743496e5f81de5
Colin Powell <colin.powell@gmail.com>
2012-09-03 01:58:05 -0400

    FIX: Remove custom admin form for slides, no more tinymce.

--- apps/superslides/forms.py
+++ apps/superslides/forms.py
@@ -1,9 +1,3 @@
 from django import forms
-from tinymce.widgets import TinyMCE
 from superslides.models import Slide
 
-class SlideAdminForm(forms.ModelForm):
-        text = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}), required=False)
-
-        class Meta:
-            model = Slide
